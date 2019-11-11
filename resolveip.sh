7445aa721439796d1de41553ee2df33de669127f
Kristaps Kaupe <kristaps.k@intelligentsystems.lv>
2019-11-11 14:38:40 +0200

    IPv6 support

--- resolveip.sh
+++ resolveip.sh
@@ -18,14 +18,15 @@ fi
 host_or_ip="$1"
 
 IPV4_MASK="[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+"
-# TODO: IPv6 support
+# IPv6 regex from https://stackoverflow.com/questions/53497/regular-expression-that-matches-valid-ipv6-addresses
+IPV6_MASK="(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"
 
 function notfoundmsg()
 {
 	(>&2 echo "$(basename $0): Unable to find hostid for '$1': host not found")
 }
 
-if grep -qsE "$IPV4_MASK" <<< "$host_or_ip"; then
+if grep -qsE "$IPV4_MASK" <<< "$host_or_ip" || grep -qsE "$IPV6_MASK" <<< "$host_or_ip"; then
 	host="$(getent hosts $host_or_ip | sed "s/\s\+/\t/g" | cut -f 2 | head -n 1)"
 	if [ "$host" == "" ]; then
 		notfoundmsg $host_or_ip
